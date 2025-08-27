09afa7b5a13f113937b974108cd9745b8c4171dc
Lucas <lucaselanobasso@gmail.com>
2025-08-27 16:23:56 -0300

    Adicionando pipeline CI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4000',
+    env: {
+      apiUrl: process.env.API_URL || 'http://localhost:3000'
+    },
     reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
